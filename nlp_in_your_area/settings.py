b06433288a109962797d357882b953879d30b344
wongself <wongself@gmail.com>
2020-11-03 21:08:49 +0800

    refresh track

--- nlp_in_your_area/settings.py
+++ nlp_in_your_area/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['101.124.42.4']
 # Application definition
 
 INSTALLED_APPS = [
+    'nlp.apps.nlpConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
